c2762d829c86c615f4df13b61a07bcf85136e8e2
wilsfred <wilsfredarmecin@yahoo.com>
2017-12-20 10:30:57 +0800

    12-20-2017 work update

--- resources/views/patientreceipt.blade.php
+++ resources/views/patientreceipt.blade.php
@@ -23,6 +23,7 @@
                 <tr>
                     <th style="text-align: left;">
                         <span style="font-size: 7pt;"><br>
+                            <b>Date :</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$totalbill->visit_date}}<br>
                             <b>Receipt # :</b> &nbsp;{{$recno}}<br>
                             <b>Name :</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$info->f_name}} {{$info->m_name}} {{$info->l_name}}<br>
                             <b>Gender :</b> &nbsp;&nbsp;&nbsp;&nbsp;{{$info->gender}}<br>
@@ -186,7 +187,7 @@
         </table>
 </div>
 
-<div>
+<!-- <div>
         <table style="width:100%;">
             <thead>
                 <tr>
@@ -194,10 +195,6 @@
                         <b style="font-size:9pt;">NEGROS FAMILY HEALTH SERVICES, INC.</b><br>
                         <span style="font-size: 7pt;">NORTH ROAD, DARO (IN FRONT OF NOPH)<br>DUMAGUETE CITY, NEGROS ORIENTAL<br>TEL No. (035)225-3544</span>
                     </th>
-                    <th style="text-align: center;">
-                        <!-- <b style="font-size:9pt;">NEGROS FAMILY HEALTH SERVICES, INC.</b><br>
-                        <span style="font-size: 7pt;">NORTH ROAD, DARO (IN FRONT OF NOPH)<br>DUMAGUETE CITY, NEGROS ORIENTAL<br>TEL No. (035)225-3544</span> -->
-                    </th>
                 </tr>
             </thead>
         </table>
@@ -212,14 +209,6 @@
                             <b>Gender :</b> &nbsp;&nbsp;&nbsp;&nbsp;{{$info->gender}}<br>
                             <b>Age :</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$info->age}}
                         </span>
-                    </th>
-                    <th style="text-align: left;">
-                        <!-- <span style="font-size: 7pt;"><br>
-                            <b>Receipt # :</b> &nbsp;{{$recno}}<br>
-                            <b>Name :</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$info->f_name}} {{$info->m_name}} {{$info->l_name}}<br>
-                            <b>Gender :</b> &nbsp;&nbsp;&nbsp;&nbsp;{{$info->gender}}<br>
-                            <b>Age :</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$info->age}}
-                        </span> -->
                     </th>
                 </tr>
             </thead>
@@ -258,7 +247,7 @@
                     </th>
 
                     <th style="text-align: left;">
-                        <!-- <b style="font-size:9pt;">SERVICES</b>
+                        <b style="font-size:9pt;">SERVICES</b>
                         <span style="font-size: 7pt;"><br>
                             @foreach($PatientService as $ap)
                                 @if($ap->admin_panel_id != 0 && $ap->admin_panel_sub_id != 0)
@@ -267,23 +256,7 @@
                                     &nbsp;&nbsp;&nbsp;{{$ap->adminP->name}}<br>
                                 @endif
                             @endforeach
-                        </span> -->
-                    </th>
-                    <th style="text-align: right;">
-                        <!-- <b style="font-size:9pt;">PRICE</b>
-                        <span style="font-size: 7pt;"><br>
-                            @foreach($PatientService as $ap)
-                                @if($ap->admin_panel_id != 0 && $ap->admin_panel_sub_id != 0)
-                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$ap->adminsubP->price}}<br>
-                                @elseif($ap->admin_panel_id != 0)
-                                    @if($ap->adminP->price == 0)
-                                        <br>
-                                    @else
-                                        &nbsp;&nbsp;&nbsp;{{$ap->adminP->price}}<br>
-                                    @endif
-                                @endif
-                            @endforeach
-                        </span> -->
+                        </span>
                     </th>
                 </tr>
             </thead>
@@ -299,13 +272,6 @@
                     <th style="text-align: right;">
                         <b style="font-size:9pt;">{{$total}}</b>
                     </th>
-
-                    <th style="text-align: left;">
-                        <!-- <b style="font-size:9pt;">AMOUNT</b> -->
-                    </th>
-                    <th style="text-align: right;">
-                        <!-- <b style="font-size:9pt;">{{$total}}</b> -->
-                    </th>
                 </tr>
             </thead>
         </table>
@@ -319,13 +285,6 @@
                     <th style="text-align: right;">
                         <b style="font-size:9pt;">{{$discounted_price}}</b>
                     </th>
-
-                    <th style="text-align: left;">
-                        <!-- <b style="font-size:9pt;">LESS DISCOUNT</b> -->
-                    </th>
-                    <th style="text-align: right;">
-                        <!-- <b style="font-size:9pt;">{{$discounted_price}}</b> -->
-                    </th>
                 </tr>
             </thead>
         </table>
@@ -337,12 +296,6 @@
                     </th>
                     <th style="text-align: right;">
                         <b style="font-size:9pt;">---------------</b>
-                    </th>
-
-                    <th style="text-align: left;">
-                    </th>
-                    <th style="text-align: right;">
-                        <!-- <b style="font-size:9pt;">---------------</b> -->
                     </th>
                 </tr>
             </thead>
@@ -358,17 +311,10 @@
                     <th style="text-align: right;">
                         <b style="font-size:9pt;">Php. {{$discounted_total}}</b>
                     </th>
-
-                    <th style="text-align: left;">
-                        <!-- <b style="font-size:9pt;">TOTAL</b> -->
-                    </th>
-                    <th style="text-align: right;">
-                        <!-- <b style="font-size:9pt;">Php. {{$discounted_total}}</b> -->
-                    </th>
                 </tr>
             </thead>
         </table>
-</div>
+</div> -->
 
 </body>
 </html>
